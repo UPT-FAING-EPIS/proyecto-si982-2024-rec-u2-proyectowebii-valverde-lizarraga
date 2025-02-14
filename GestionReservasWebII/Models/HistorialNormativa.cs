0a417b09ff47077b9747a43fcf6e0f95ca649454
JeanValverde24 <jv2020066920@virtual.upt.pe>
2025-02-14 17:38:43 -0500

    Prueba del sonar

--- GestionReservasWebII/Models/HistorialNormativa.cs
+++ GestionReservasWebII/Models/HistorialNormativa.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-//ejemplosasdasdsadsaclsdsadsarewrewkjkkjkj
+//ejemplosasdasdsadsaclsdsadsarewrewkjkkjkjsdfdsfds
 namespace GestionReservasWebII.Models;
 
 public partial class HistorialNormativa
