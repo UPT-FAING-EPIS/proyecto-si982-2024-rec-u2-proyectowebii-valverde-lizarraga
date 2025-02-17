814607b87c5b1ce1f413c533ba9d23436aa3b001
Jean-Valverde <90206909+JeanValverde24@users.noreply.github.com>
2025-02-17 16:38:38 -0500

    Update HistorialNormativa.cs

--- GestionReservasWebII/Models/HistorialNormativa.cs
+++ GestionReservasWebII/Models/HistorialNormativa.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-//ejemplosasdasdsadsaclsdsadsarewrewkjkkjkjsdfdsfdsddsadsagitssssadsrewrewdsdsaDSADSA
+//ejemplosasdasdsadsaclsdsadsarewrewkjkkjkjsdfdsfdsddsadsagitssssadsrewrewdsdsaDSADSAAS
 namespace GestionReservasWebII.Models;
 
 public partial class HistorialNormativa
